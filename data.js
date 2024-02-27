54ef65868d536d76bcc72cae94a58d23f84d6e76
Christian Lane <christianm3870@gmail.com>
2024-02-27 17:01:47 -0500

    added basic login functionality

--- data.js
+++ data.js
@@ -1,20 +1,21 @@
 const dataLocation = "https:///jsonblob.com/api/jsonBlob/jsonblob.com/1212135446795902976"
 const itemsPerPage = 9
 
-
+var data;
+var userShown = false;
 async function loadIndex() {
     try {
         const response = await fetch(dataLocation);
         data = await response.json();
-        displayIndexData(0, itemsPerPage, false);
     } catch (error) {
         console.error('Failed to load data!', error);
     }
+
 }
 
-function displayIndexData() {
+function displayIndexData(user) {
     // until more work is done, i'm sticking to a single user
-    userData = data["cassiancc"]
+    userData = data[user]
     console.log()
     document.getElementById("title-div").innerHTML += `<h2>${userData.username}</h2>`
     
@@ -38,3 +39,18 @@ function displayIndexData() {
     
     document.getElementById("transaction-history").innerHTML += userData.transactionHistory[0]
 }
+
+$(document).on("click", "#login-button", function() {
+    username = $("#username").val();
+    console.log(username)
+
+    if(!userShown){
+        if (username in data) {  
+            console.log("success!");
+            userShown = true;
+            $("#content").css("display", "block");
+            $("#login").css("display", "none");
+            displayIndexData(username);
+        }
+    }
+});
