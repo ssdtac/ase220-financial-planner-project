1b465d7be48d82a85fca0454c7dd4b5731fbcda5
Cassian Godsted <cassian@godsted.com>
2024-02-27 16:00:48 -0500

    Update table from Jsonblob Data

--- data.js
+++ data.js
@@ -1,7 +1,6 @@
 const dataLocation = "https:///jsonblob.com/api/jsonBlob/jsonblob.com/1212135446795902976"
 const itemsPerPage = 9
 
-var data;
 
 async function loadIndex() {
     try {
@@ -18,4 +17,24 @@ function displayIndexData() {
     userData = data["cassiancc"]
     console.log()
     document.getElementById("title-div").innerHTML += `<h2>${userData.username}</h2>`
+    
+    userData.transactionHistory.forEach(function(transaction) {
+        let frequency;
+        if (transaction.recurring = true) {
+            frequency = "Recurring"
+        }
+        else {
+            frequency = "One-Time"
+        }
+        document.querySelector("tbody").innerHTML += `
+        <tr>
+        <td>${transaction.date}</td>
+        <td>${transaction.type}</td>
+        <td>${transaction.vendor}</td>
+        <td>$${transaction.amount}</td>
+        <td>${frequency}</td>
+        </tr>`
+    })
+    
+    document.getElementById("transaction-history").innerHTML += userData.transactionHistory[0]
 }
