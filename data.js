3913296b5b1200220132eaaed932b51b56b3e643
Cassian Godsted <cassian@godsted.com>
2024-02-27 15:34:21 -0500

    Add JSON structure

--- data.js
+++ data.js
@@ -1,2 +1,2 @@
-const dataLocation = "https:///jsonblob.com/api/jsonBlob/jsonblob.com/1209608571263508480"
+const dataLocation = "https:///jsonblob.com/api/jsonBlob/jsonblob.com/1212135446795902976"
 
