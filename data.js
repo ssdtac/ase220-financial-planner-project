21dc67e2d713d0adde5bf2d594abe97ca071bbc3
Cassian Godsted <cassian@godsted.com>
2024-02-27 15:41:39 -0500

    Basic demo of loading from Jsonblob

--- data.js
+++ data.js
@@ -1,2 +1,20 @@
 const dataLocation = "https:///jsonblob.com/api/jsonBlob/jsonblob.com/1212135446795902976"
+const itemsPerPage = 9
 
+
+async function loadIndex() {
+    try {
+        const response = await fetch(dataLocation);
+        data = await response.json();
+        displayIndexData(0, itemsPerPage, false);
+    } catch (error) {
+        console.error('Failed to load data!', error);
+    }
+}
+
+function displayIndexData() {
+    // until more work is done, i'm sticking to a single user
+    userData = data[0]
+    console.log()
+    document.getElementById("title-div").innerHTML += `<h2>${userData.username}</h2>`
+}
